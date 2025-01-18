2dccfae1fb3913dcfef50c907c9f3a642a215888
ItaloCosta-dev <italocosta.dev@gmail.com>
2025-01-18 10:25:21 -0300

    feat: tela de login social criada.

--- app/social-login/page.tsx
+++ app/social-login/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
+
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <p>login social</p>
+    <div className="flex items-center justify-center h-screen p-3">
+      <div className="flex flex-col items-center justify-center p-5 rounded-sm gap-3">
+        <h1 className="text-4xl font-bold">Acesse o planit</h1>
+        <Button className="bg-white border">
+          <FcGoogle />
+          <p className="text-black">Faça login com Google</p>
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
