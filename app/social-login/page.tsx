3c11dc0d7582b6db2513b1358efbd4aba05fac52
ItaloCosta-dev <italocosta.dev@gmail.com>
2025-01-20 11:17:38 -0300

    feat: header da aplicação principal concluído.

--- app/social-login/page.tsx
+++ app/social-login/page.tsx
@@ -2,20 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
-import { GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import { auth } from "../services/firebase";
-import { useState } from "react";
-import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-const Page = () => {
-  const [user, setUser] = useState<User | null>(null);
+const LoginPage = () => {
+  const router = useRouter();
 
   function handleGoogleSignIn() {
     const provider = new GoogleAuthProvider();
 
     signInWithPopup(auth, provider)
-      .then((result) => {
-        setUser(result.user);
+      .then(() => {
+        router.push("/task-page");
       })
       .catch((error) => {
         console.error(error);
@@ -26,27 +25,16 @@ const Page = () => {
     <div className="flex items-center justify-center h-screen p-3">
       <div className="flex flex-col items-center justify-center p-5 rounded-sm gap-3">
         <h1 className="text-4xl font-bold">Acesse o Planit</h1>
-        <Button className="bg-white border flex items-center gap-2" onClick={handleGoogleSignIn}>
+        <Button
+          className="bg-white border flex items-center gap-2"
+          onClick={handleGoogleSignIn}
+        >
           <FcGoogle size={24} />
           <p className="text-black">Faça login com Google</p>
         </Button>
-        {user && (
-          <div className="flex items-center gap-4 mt-4">
-            {user.photoURL && (
-              <Image
-                src={user.photoURL}
-                alt={user.displayName || "Foto do usuário"}
-                width={50}
-                height={50}
-                className="rounded-full"
-              />
-            )}
-            <strong className="text-lg">{user.displayName}</strong>
-          </div>
-        )}
       </div>
     </div>
   );
 };
 
-export default Page;
+export default LoginPage;
---
+++ app/task-page/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { IoMdExit } from "react-icons/io";
+import { User } from "firebase/auth";
+import { auth } from "../services/firebase";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+const TaskPage = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+   const router = useRouter();
+  
+    const handleNavigation = () => {
+      router.push("/");
+    };
+
+  useEffect(() => {
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      setUser(currentUser);
+    }
+  }, []);
+
+  return (
+    <div>
+      <header className="bg-slate-200 flex flex-row justify-between items-center p-2">
+        <div>
+          <span className="bg-slate-700 p-2 rounded-md text-white font-bold">
+            planit
+          </span>
+        </div>
+
+        <div>
+          <h1 className="font-semibold">Bem vindo ao planit</h1>
+        </div>
+
+        <div className="flex flex-row gap-2 justify-center items-center">
+          {user ? (
+            <>
+              <span className="font-medium">{user.displayName}</span>
+              {user.photoURL && (
+                <Image
+                  src={user.photoURL}
+                  alt={`Perfil de ${user.displayName}`}
+                  width={32}
+                  height={32}
+                  className="rounded-sm"
+                />
+              )}
+              <Button className="bg-slate-700"  onClick={handleNavigation}><IoMdExit size={20} /></Button>
+              
+            </>
+          ) : (
+            <span>Usuário não encontrado</span>
+          )}
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default TaskPage;
