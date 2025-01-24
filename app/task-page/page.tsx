d8ad69e61ec98bbc0aa75a640fb9f42332b65bd3
ItaloCosta-dev <italocosta.dev@gmail.com>
2025-01-24 10:25:12 -0300

    feat: header adaptado para mobile.

--- app/task-page/page.tsx
+++ app/task-page/page.tsx
@@ -4,18 +4,28 @@ import React, { useState, useEffect } from "react";
 import { IoMdExit } from "react-icons/io";
 import { User } from "firebase/auth";
 import { auth } from "../services/firebase";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { FaUser } from "react-icons/fa";
+
+import Image from "next/image";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const TaskPage = () => {
   const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
 
-   const router = useRouter();
-  
-    const handleNavigation = () => {
-      router.push("/");
-    };
+  const handleNavigation = () => {
+    router.push("/");
+  };
 
   useEffect(() => {
     const currentUser = auth.currentUser;
@@ -28,34 +38,62 @@ const TaskPage = () => {
     <div>
       <header className="bg-slate-200 flex flex-row justify-between items-center p-2">
         <div>
-          <span className="bg-slate-700 p-2 rounded-md text-white font-bold">
+          <span className="p-2 rounded-md text-slate-700 font-bold">
             planit
           </span>
         </div>
 
-        <div>
+        <div className="hidden sm:block">
           <h1 className="font-semibold">Bem vindo ao planit</h1>
         </div>
 
-        <div className="flex flex-row gap-2 justify-center items-center">
-          {user ? (
-            <>
-              <span className="font-medium">{user.displayName}</span>
-              {user.photoURL && (
-                <Image
-                  src={user.photoURL}
-                  alt={`Perfil de ${user.displayName}`}
-                  width={32}
-                  height={32}
-                  className="rounded-sm"
-                />
-              )}
-              <Button className="bg-slate-700"  onClick={handleNavigation}><IoMdExit size={20} /></Button>
-              
-            </>
-          ) : (
-            <span>Usuário não encontrado</span>
-          )}
+        <div className="flex flex-row gap-2 items-center">
+          <div className="hidden sm:flex flex-row gap-2 items-center">
+            {user ? (
+              <>
+                <span className="font-medium">{user.displayName}</span>
+                {user.photoURL && (
+                  <Image
+                    src={user.photoURL}
+                    alt={`Perfil de ${user.displayName}`}
+                    width={32}
+                    height={32}
+                    className="rounded-sm"
+                  />
+                )}
+                <Button
+                  className="bg-slate-700"
+                  onClick={handleNavigation}
+                >
+                  <IoMdExit size={20} />
+                </Button>
+              </>
+            ) : (
+              <span>Usuário não encontrado</span>
+            )}
+          </div>
+
+          {/* Menu Mobile */}
+          <div className="sm:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button className="p-2 bg-slate-700 text-white"><FaUser /></Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {user ? (
+                  <>
+                    <DropdownMenuLabel>Olá, {user.displayName}</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={handleNavigation} className="cursor-pointer">
+                      Sair
+                    </DropdownMenuItem>
+                  </>
+                ) : (
+                  <DropdownMenuItem>Usuário não encontrado</DropdownMenuItem>
+                )}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </header>
       <h1>CALMA PAIZÃO, TÔ CONSTRUINDO</h1>
