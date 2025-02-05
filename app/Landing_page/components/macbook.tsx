61e122e32a0fd0746fd2fe63798ed01e1b2b6950
ItaloCosta-dev <italocosta.dev@gmail.com>
2025-02-05 08:16:48 -0300

    feat: ajustes na landing

--- app/Landing_page/components/macbook.tsx
+++ app/Landing_page/components/macbook.tsx
@@ -3,9 +3,16 @@ import Image from "next/image";
 const Macbook = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-5 mt-5">
-       <Image src="/macbook.png" alt="Planit" width={1000} height={500} />
-    </div>
-  )
-}
+      <div className="max-w-md mx-auto p-4">
+        <h1 className="font-bold text-lg sm:text-xl md:text-3xl text-center p-5">
+          Melhore a produtividade com uma lista de tarefas 100% gratuita para
+          usar no computador e no seu celular.
+        </h1>
+      </div>
 
-export default Macbook
+      <Image src="/macbook.png" alt="Planit" width={1000} height={500} />
+    </div>
+  );
+};
+
+export default Macbook;
